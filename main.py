3718cc9fd9a8608ff50f2ec6cdb59ae117dbc625
Jorge Delgado <jorgeadm4@gmail.com>
2023-01-27 13:49:29 -0600

    Comments

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from time import sleep
 
 while (True):
     try:
-        pwk.sendwhats_image("+522228097257", 
-            "brett.jpg",
-            "Toma awa y no procrastines :3",
-            tab_close=True)
+        pwk.sendwhats_image("+522228097257", # Numero destino
+            "brett.jpg", # ubicacion de imagen
+            "Toma awa y no procrastines :3", # caption
+            tab_close=True) # cerrar ventana despues de mensaje
     except Exception as e:
         print(f"Error {e}")
 
+    # Repite cada hora (argumento en segundos)
     sleep(3600)
